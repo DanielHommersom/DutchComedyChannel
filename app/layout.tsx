71883488f250f89c73a365962d0f28fa53b6f54c
Daniël Hommersom <danielhommersom@MacBook-Pro-van-Daniel.local>
2026-03-05 23:07:23 +0100

    added google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import ThemeRegistry from "@/components/ThemeRegistry";
 import Navbar from "@/components/layout/Navbar";
@@ -21,6 +22,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="nl">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3366446717708247"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body>
         <AppRouterCacheProvider>
           <ThemeRegistry>
